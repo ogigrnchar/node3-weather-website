e29ec55db053545f0d4e65b0fd64a278f95b4964
Ogi <ogigrnchar@gmail.com>
2021-02-16 02:19:31 +1100

    Added humidity display.

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location. Try another search.', undefined);
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees outside. It feels like ' + body.current.feelslike + ' degrees. There is a ' + body.current.precip + '% chance of rain.');
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees outside. It feels like ' + body.current.feelslike + ' degrees. There is a ' + body.current.precip + '% chance of rain. The humidity is ' + body.current.humidity + "%.");
         }
     })
 }
